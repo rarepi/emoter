643f79caf1744bd990ef3473d3a6e7ab98a30c13
Shiki <elpingu42@gmail.com>
2020-08-10 17:31:48 +0200

    GUI: Added Sheet File Selection; Reworked window setup; Did some refactoring

--- emoter_gui.py
+++ emoter_gui.py
@@ -1,7 +1,12 @@
 from tkinter import *
+from tkinter import filedialog
+
 from PIL import Image
 from PIL import ImageTk
 from PIL import ImageDraw
+
+from pathlib import Path
+
 from emoter import make_emotes
 
 
@@ -10,7 +15,8 @@ class emoter_gui:
         super().__init__()
         self.sprite_size = 0
         self.emote_size = 96
-        self.img = None
+        self.image_file = None
+        self.sprite_sheet = None
         self.sprite_rows = 0
         self.sprite_cols = 0
         self.rect = None
@@ -18,73 +24,97 @@ class emoter_gui:
         self.y = 0
 
     def initUI(self):
-
+        h = 512
+        w = 1024
         root = Tk()
         root.title('Emoter')
+        root.geometry(str(w + 50) + "x" + str(h + 50))
 
-        self.img = "Jeanne_DArc_Alter_Berserker.png"
-        img = Image.open(self.img)
-        img_tk = ImageTk.PhotoImage(image=img)
+        ####################
+        # Open File Dialog #
+        ####################
+        # canvas
+        file_dialog_canvas = Canvas(root, width=w, height=50)
+        file_dialog_canvas.pack(side=TOP, expand=True, fill=BOTH)
+        # input
+        svar_image_file = StringVar(file_dialog_canvas, value="")
+        entry_image_file = Entry(file_dialog_canvas, textvariable=svar_image_file)
 
+        # button
+        def open_image_file():
+            svar_image_file.set(filedialog.askopenfilename(initialdir=".", title="Select file"))
+            update_image_file()
+        button_open_image_file = Button(file_dialog_canvas, text="Open", command=open_image_file)
+        button_open_image_file.pack(side=LEFT)
+        entry_image_file.pack(expand=True, fill=BOTH, padx=10, pady=10)
 
-        h = 800
-        w = h
-        frame = Frame(root,width=w,height=h)
+        def update_image_file(event=None):
+            self.image_file = svar_image_file.get()
+            self.sprite_sheet = PhotoImage(file=self.image_file)
+            canvas.itemconfig("sprite-sheet", image=self.sprite_sheet)
+            canvas.config(scrollregion=(0, 0, self.sprite_sheet.width(), self.sprite_sheet.height()))
+            self.sprite_cols = int(self.sprite_sheet.width() / self.sprite_size)
+            self.sprite_rows = int(self.sprite_sheet.height() / self.sprite_size)
+            canvas.delete("rect")
+            for i in range(0, self.sprite_cols + 1):
+                for j in range(0, self.sprite_rows + 1):
+                    canvas.create_image(i * self.sprite_size, j * self.sprite_size, anchor=NW,
+                                        tags=("rect", "rect" + str(i) + "-" + str(j)))
+            render_rectangles()
+
+        entry_image_file.bind("<Return>", update_image_file)
+        entry_image_file.bind("<FocusOut>", update_image_file)
+
+        ################
+        # Sheet Canvas #
+        ################
+        frame = Frame(root, width=w, height=h)
         frame.pack(expand=True, fill=BOTH)
-        canvas = Canvas(frame, width=img_tk.width(), height=h, bg="white", scrollregion=(0,0,img_tk.width(),img_tk.height()))
-        vbar = Scrollbar(frame,orient=VERTICAL)
-        vbar.pack(side=RIGHT,fill=Y)
+        canvas = Canvas(frame, width=w, height=h, bg="white")
+        canvas.create_image(0, 0, anchor=NW, tag="sprite-sheet")
+
+        vbar = Scrollbar(frame, orient=VERTICAL)
+        vbar.pack(side=RIGHT, fill=Y)
         vbar.config(command=canvas.yview)
-        canvas.config(width=img_tk.width(),height=300)
-        canvas.config(yscrollcommand=vbar.set)
-        canvas.pack(side=LEFT,expand=True,fill=Y)
+        hbar = Scrollbar(frame, orient=HORIZONTAL)
+        hbar.pack(side=BOTTOM, fill=X)
+        hbar.config(command=canvas.xview)
+        canvas.config(yscrollcommand=vbar.set, xscrollcommand=hbar.set)
+        canvas.pack(side=LEFT, expand=True, fill=Y)
         canvas.pack(pady=10)
-        img_canvas = canvas.create_image(0, 0, anchor=NW, image=img_tk)
-
-
-        def confirm():
-            make_emotes(self.img, self.sprite_size, self.x, self.y, self.emote_size)
 
         config_canvas = Canvas(frame, width=50, height=h)
-        config_canvas.pack(side=RIGHT,expand=True,fill=BOTH)
+        config_canvas.pack(side=RIGHT, expand=True, fill=BOTH)
 
-        sv_sprite_size = IntVar(config_canvas, value=256)
-        self.sprite_size = sv_sprite_size.get()
-        entry_sprite_size = Entry(config_canvas, textvariable=sv_sprite_size)
+        ivar_sprite_size = IntVar(config_canvas, value=256)
+        self.sprite_size = ivar_sprite_size.get()
+        entry_sprite_size = Entry(config_canvas, textvariable=ivar_sprite_size)
         entry_sprite_size.pack()
 
-        self.sprite_cols = int(img.width/self.sprite_size)
-        self.sprite_rows = int(img.height/self.sprite_size)
-
         def update_sprite_size(event):
-            print(str(event))
-            self.sprite_size = sv_sprite_size.get()
+            self.sprite_size = ivar_sprite_size.get()
+            # TODO reset rectangle positions here
 
         entry_sprite_size.bind("<Return>", update_sprite_size)
         entry_sprite_size.bind("<FocusOut>", update_sprite_size)
 
-
-
-        sv_emote_size = IntVar(config_canvas, value=96)
-        self.emote_size = sv_emote_size.get()
-        entry_emote_size = Entry(config_canvas, textvariable=sv_emote_size)
+        ivar_emote_size = IntVar(config_canvas, value=96)
+        self.emote_size = ivar_emote_size.get()
+        entry_emote_size = Entry(config_canvas, textvariable=ivar_emote_size)
         entry_emote_size.pack()
 
         def update_emote_size(event):
-            print(str(event))
-            self.emote_size = sv_emote_size.get()
+            self.emote_size = ivar_emote_size.get()
             render_rectangles()
 
         entry_emote_size.bind("<Return>", update_emote_size)
         entry_emote_size.bind("<FocusOut>", update_emote_size)
 
-
+        def confirm():
+            make_emotes(self.image_file, self.sprite_size, self.x, self.y, self.emote_size)
 
         b = Button(config_canvas, text="OK", command=confirm)
         b.pack()
-
-        root.geometry(str(img.width+50) + "x" + str(h))
-        canvas.config(width=img.width, height=img.height)
 
         def render_rectangles():
             alpha = Image.new("RGBA", (self.emote_size, self.emote_size), (255, 255, 255, 0))
@@ -96,15 +126,9 @@ class emoter_gui:
             self.rect = ImageTk.PhotoImage(image=alpha)
             canvas.itemconfig("rect", image=self.rect)
 
-        for i in range(0, self.sprite_cols+1):
-            for j in range(0, self.sprite_rows+1):
-                canvas.create_image(i*self.sprite_size, j*self.sprite_size, anchor=NW, tags=("rect", "rect" + str(i) + "-" + str(j)))
-        render_rectangles()
-
-
         def move(event):
-            self.x = event.x % self.sprite_size
-            self.y = event.y % self.sprite_size
+            self.x = min(max(0, event.x), w - 1) % self.sprite_size
+            self.y = min(max(0, event.y), h - 1) % self.sprite_size
 
             if self.x + self.emote_size > self.sprite_size:
                 self.x = self.sprite_size - self.emote_size
@@ -114,10 +138,10 @@ class emoter_gui:
             # label.config(text=str(event.x) + " / " + str(event.y))
             for i in range(0, self.sprite_cols + 1):
                 for j in range(0, self.sprite_rows + 1):
-                    canvas.coords("rect" + str(i) + "-" + str(j), self.x+i*self.sprite_size, self.y+j*self.sprite_size)
+                    canvas.coords("rect" + str(i) + "-" + str(j), self.x + i * self.sprite_size,
+                                  self.y + j * self.sprite_size)
+
         canvas.bind("<B1-Motion>", move)
-
-
 
         root.mainloop()
 
